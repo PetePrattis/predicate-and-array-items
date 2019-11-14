db2d055d7581598957f5e8b5f149ef7977d45cb9
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-14 23:41:26 +0200

    Update list.pl

--- list.pl
+++ list.pl
@@ -4,7 +4,7 @@
 :- format('Type "common_list(List1, List2)" with List1, List2 being 2 random lists of your choice ~n'),
 	format('this way you will see if these 2 lists have any common values ~n').
  
-common_list(X, Y) :- /*το κατηγόρημα common_list(X,Y) θα είναι */
+common_list(X, Y) :- /*the predicate common_list (X, Y) will be */
 	member(Value, X), /* A random variable with a value that belongs to the 1st list */
 	member(Value, Y), /* To belong to the 2nd list at the same time, that is, a value that is common */
 	/* The Value variable only takes values from the X list and is controlled retrospectively
