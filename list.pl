6cc539103cd682025a6fbb4ed2c7146eed81ae58
Panagiotis Prattis <panagiotispr1997@gmail.com>
2019-11-14 23:40:53 +0200

    Update list.pl

--- list.pl
+++ list.pl
@@ -1,20 +1,13 @@
-/**----------------------------------------------------------------------------------------
-2η προαιρειτική εργασία. Παράδοση μέσω gunet2 μέχρι 29 Δεκεμβρίου 2017. 
-Παραδοτέο είναι τεκμηριωμένος κώδικας και παράδειγμα σωστής εκτέλεσης.
-Να γραφτεί το κατηγόρημα common_list(X, Y) που αληθεύει 
-όταν οι δύο λίστες έχουν τουλάχιστον ένα κοινό στοιχείο. 
------------------------------------------------------------------------------------------*/
+% Παναγιώτης Πράττης / Panagiotis Prattis
 
-% Παναγιώτης Πράττης Π15120
-
-/* τυπώνω οδηγίες με την εκτέλεση*/
+/* print instructions with execution*/
 :- format('Type "common_list(List1, List2)" with List1, List2 being 2 random lists of your choice ~n'),
 	format('this way you will see if these 2 lists have any common values ~n').
  
 common_list(X, Y) :- /*το κατηγόρημα common_list(X,Y) θα είναι */
-	member(Value, X), /* Μια τυχαία μεταβλητή με τιμή που ανήκει στην 1η λίστα */
-	member(Value, Y), /* Να ανήκει ταυτόχρονα και στην 2η λίστα, δηλαδή μια τιμή που είναι κοινή */
-	/* Η Value μεταβλητή παίρνει μόνο τιμές που ανήκουν στην λίστα Χ και ελέγχεται αναδρομικά
-	αν ανήκει κάθε μια από αυτές τις τιμές της λίστας Χ και στην λίστα Υ */
-	format('"~w" is a common value ~n', [Value]). /* εκτυπώνω αυτήν την τιμή αν υπάρχει για κάθε αναδρομή */
+	member(Value, X), /* A random variable with a value that belongs to the 1st list */
+	member(Value, Y), /* To belong to the 2nd list at the same time, that is, a value that is common */
+	/* The Value variable only takes values from the X list and is controlled retrospectively
+	if each of these values of list X also belongs to list Y */
+	format('"~w" is a common value ~n', [Value]). /* print this value if there is any recursion */
 
